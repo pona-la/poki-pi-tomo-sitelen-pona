7745c071a316b5294a34dd5dad28443e53499a05
Janet Blackquill <uhhadd@gmail.com>
2024-04-12 23:39:15 -0400

    chunk up the data

--- aggregate.mjs
+++ aggregate.mjs
@@ -5,7 +5,12 @@ const res = {}
 
 for (const dir of fs.readdirSync("families")) {
 	const it = JSON.parse(fs.readFileSync(`families/${dir}/metadata.json`))
+	for (const font of it.fonts) {
+		delete font.characters
+	}
+	const it2 = JSON.parse(fs.readFileSync(`families/${dir}/metadata.json`))
+	fs.writeFileSync(`families/${dir}/metadata_compressed.json`, JSON.stringify(compress(it2), null, "\t"))
 	res[dir] = it
 }
 
-fs.writeFileSync(`all.json`, JSON.stringify(compress(res), null, "\t"))
+fs.writeFileSync(`index.json`, JSON.stringify(res, null, "\t"))

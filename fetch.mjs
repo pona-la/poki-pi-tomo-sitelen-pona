da0331b5acd99e02e18184314df2a9be2d6a3bb9
Janet Blackquill <uhhadd@gmail.com>
2024-04-19 13:17:09 -0400

    undeprecate nasin luka

--- fetch.mjs
+++ fetch.mjs
@@ -41,7 +41,7 @@ async function handleFamily(dir) {
 	}
 }
 
-handleFamily("CraftyPE")
+handleFamily("nasin luka")
 
 // for (const dir of fs.readdirSync("families")) {
 // 	if (dir == "linja wawa") {
